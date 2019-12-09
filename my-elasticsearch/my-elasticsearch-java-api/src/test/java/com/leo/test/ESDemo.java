e8fedd58e4fa1dc9edb897c627e91216fd1d9001
Administrator <Administrator@SKY-20191107UYE>
2019-12-10 00:28:45 +0800

    05-ES-java-api

--- my-elasticsearch/my-elasticsearch-java-api/src/test/java/com/leo/test/ESDemo.java
+++ my-elasticsearch/my-elasticsearch-java-api/src/test/java/com/leo/test/ESDemo.java
@@ -1,11 +1,17 @@
 package com.leo.test;
 
 import java.net.InetAddress;
+import java.util.Map;
 
+import org.elasticsearch.action.bulk.BulkRequestBuilder;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.get.MultiGetItemResponse;
+import org.elasticsearch.action.get.MultiGetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
@@ -13,6 +19,21 @@ import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.reindex.BulkByScrollResponse;
+import org.elasticsearch.index.reindex.DeleteByQueryAction;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilder;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.filter.Filter;
+import org.elasticsearch.search.aggregations.bucket.filter.Filters;
+import org.elasticsearch.search.aggregations.bucket.filter.FiltersAggregator;
+import org.elasticsearch.search.aggregations.bucket.range.Range;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import org.elasticsearch.search.aggregations.metrics.max.Max;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.junit.Test;
 
@@ -116,40 +137,370 @@ public class ESDemo {
 		System.out.println(response.status());
 		client.close();
 	}
-
+	
+	//批量查询
 	@Test
 	public void test6() throws Exception {
+		Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.182.101"), 9300));
+		
+		MultiGetResponse response = client.prepareMultiGet()
+				.add("index1", "blog", "8")
+				.add("index1", "blog", "8","1")
+				.get();
 
+		for(MultiGetItemResponse item:response) {
+			GetResponse gr = item.getResponse();
+			if(gr!=null && gr.isExists()) {
+				System.out.println(gr.getSourceAsString());
+			}
+		}
+		client.close();
 	}
 
+	// bulk批量操作
 	@Test
 	public void test7() throws Exception {
-
+		Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.182.101"), 9300));
+		
+		BulkRequestBuilder	 bulkRequestBuilder= client.prepareBulk();
+		
+		//批量添加
+		bulkRequestBuilder.add(client.prepareIndex("index1", "blog", "8").setSource(XContentFactory.jsonBuilder().startObject()
+				.field("title","python")
+				.field("price", 99)
+				.endObject()));
+		
+		bulkRequestBuilder.add(client.prepareIndex("index1", "blog", "1").setSource(XContentFactory.jsonBuilder()
+				.startObject()
+				.field("title", "VR")
+				.field("price",29)
+				.endObject()));
+		
+		BulkResponse response = bulkRequestBuilder.get();
+		System.out.println(response.status());
+		if(response.hasFailures()) {
+			System.out.println("失败了");
+		}
+		client.close();
 	}
 
+	// 查询删除
 	@Test
 	public void test8() throws Exception {
-
+		Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.182.101"), 9300));
+		
+		BulkByScrollResponse response = DeleteByQueryAction.INSTANCE
+				.newRequestBuilder(client)
+				.filter(QueryBuilders.matchQuery("title", "工厂"))
+				.source("index1")
+				.get();
+		
+		long counts = response.getDeleted();
+		System.out.println(counts);
 	}
 
+	// match_all
 	@Test
 	public void test9() throws Exception {
-
+		Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.182.101"), 9300));
+		QueryBuilder qb = QueryBuilders.matchAllQuery();
+		SearchResponse sr = client.prepareSearch("index1")
+					.setQuery(qb)
+					.setSize(3)
+					.get();
+		SearchHits hits = sr.getHits();
+		
+		for (SearchHit searchHit : hits) {
+			System.out.println(searchHit.getSourceAsString());
+			Map<String, Object> map = searchHit.getSourceAsMap();
+			for (String key : map.keySet()) {
+				System.out.println(key+"="+map.get(key));
+			}
+		}
+		client.close();
 	}
-
+	// match query
 	@Test
 	public void test10() throws Exception {
-
+		Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.182.101"), 9300));
+		QueryBuilder qb = QueryBuilders.matchQuery("interests", "changge");
+		SearchResponse sr = client.prepareSearch("blog")
+				.setQuery(qb)
+				.setSize(3)
+				.get();
+		SearchHits hits = sr.getHits();
+		for (SearchHit searchHit : hits) {
+			System.out.println(searchHit.getSourceAsString());
+			Map<String, Object> map = searchHit.getSourceAsMap();
+			for (String key : map.keySet()) {
+				System.out.println(key+"="+map.get(key));
+			}
+		}
+		client.close();
 	}
 
+	// multi match query
 	@Test
 	public void test11() throws Exception {
-
+		Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.182.101"), 9300));
+		QueryBuilder qb = QueryBuilders.multiMatchQuery( "changge","interests","address");
+		SearchResponse sr = client.prepareSearch("blog")
+				.setQuery(qb)
+				.setSize(3)
+				.get();
+		SearchHits hits = sr.getHits();
+		for (SearchHit searchHit : hits) {
+			System.out.println(searchHit.getSourceAsString());
+			Map<String, Object> map = searchHit.getSourceAsMap();
+			for (String key : map.keySet()) {
+				System.out.println(key+"="+map.get(key));
+			}
+		}
+		client.close();
 	}
 
+	// term
 	@Test
 	public void test12() throws Exception {
-
+		Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.182.101"), 9300));
+		QueryBuilder qb = QueryBuilders.termQuery("interests", "changge");
+		SearchResponse sr = client.prepareSearch("blog")
+				.setQuery(qb)
+				.setSize(2)
+				.get();
+		SearchHits hits = sr.getHits();
+		for (SearchHit searchHit : hits) {
+			System.out.println(searchHit.getSourceAsString());
+			Map<String, Object> map = searchHit.getSourceAsMap();
+			for (String key : map.keySet()) {
+				System.out.println(key+"="+map.get(key));
+			}
+		}
+		client.close();
 	}
-
+	
+	// terms 
+	@Test
+	public void test13() throws Exception {
+		Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.182.101"), 9300));
+		QueryBuilder qb = QueryBuilders.termsQuery("interests", "changge","lvyou");
+		SearchResponse sr = client.prepareSearch("blog")
+				.setQuery(qb)
+				.setSize(2)
+				.get();
+		SearchHits hits = sr.getHits();
+		for (SearchHit searchHit : hits) {
+			System.out.println(searchHit.getSourceAsString());
+			Map<String, Object> map = searchHit.getSourceAsMap();
+			for (String key : map.keySet()) {
+				System.out.println(key+"="+map.get(key));
+			}
+		}
+		client.close();
+	}
+	@Test
+	public void test14() throws Exception {
+		Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.182.101"), 9300));
+		// range查询
+		//QueryBuilder qb = QueryBuilders.rangeQuery("birthday").from("1990-01-01").to("2000-12-31");
+		// prefix查询
+		//QueryBuilder qb = QueryBuilders.prefixQuery("name","zhao");
+		//wildcard查询
+		//QueryBuilder qb = QueryBuilders.wildcardQuery("name","zhao*");
+		//fuzzy查询
+		//QueryBuilder qb = QueryBuilders.fuzzyQuery("name","za");
+		//ids查询
+		QueryBuilder qb = QueryBuilders.idsQuery().addIds("1","2");
+		SearchResponse sr = client.prepareSearch("blog")
+				.setQuery(qb)
+				.setSize(2)
+				.get();
+		SearchHits hits = sr.getHits();
+		for (SearchHit searchHit : hits) {
+			System.out.println(searchHit.getSourceAsString());
+			Map<String, Object> map = searchHit.getSourceAsMap();
+			for (String key : map.keySet()) {
+				System.out.println(key+"="+map.get(key));
+			}
+		}
+		client.close();
+	}
+	
+	// 聚合查询 计数、最大值、最小值、平均值，综合
+	@Test
+	public void test15() throws Exception {
+		Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.182.101"), 9300));
+		AggregationBuilder agg = AggregationBuilders.max("aggMax").field("age");
+		SearchResponse response = client.prepareSearch("index1").addAggregation(agg).get();
+		Max max = response.getAggregations().get("aggMax");
+		System.out.println(max.getValue());
+		client.close();
+	}
+	
+	// query string
+	@Test
+	public void test16() throws Exception {
+		Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.182.101"), 9300));
+		QueryBuilder qb = QueryBuilders.commonTermsQuery("interests", "changge");
+		SearchResponse sr = client.prepareSearch("blog")
+				.setQuery(qb)
+				.setSize(2)
+				.get();
+		SearchHits hits = sr.getHits();
+		for (SearchHit searchHit : hits) {
+			System.out.println(searchHit.getSourceAsString());
+			Map<String, Object> map = searchHit.getSourceAsMap();
+			for (String key : map.keySet()) {
+				System.out.println(key+"="+map.get(key));
+			}
+		}
+		client.close();
+	}
+	@Test
+	public void test17() throws Exception {
+		Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.182.101"), 9300));
+		// 包含changge，不包含hejiu,可以不同时满足
+		// QueryBuilder qb = QueryBuilders.queryStringQuery("+changge -hejiu");
+		// 同时满足
+		QueryBuilder qb = QueryBuilders.simpleQueryStringQuery("+changge -hejiu");
+		SearchResponse sr = client.prepareSearch("blog")
+				.setQuery(qb)
+				.setSize(2)
+				.get();
+		SearchHits hits = sr.getHits();
+		for (SearchHit searchHit : hits) {
+			System.out.println(searchHit.getSourceAsString());
+			Map<String, Object> map = searchHit.getSourceAsMap();
+			for (String key : map.keySet()) {
+				System.out.println(key+"="+map.get(key));
+			}
+		}
+		client.close();
+	}
+	
+	// 组合查询
+	@Test
+	public void test18() throws Exception {
+		Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.182.101"), 9300));
+		QueryBuilder qb = QueryBuilders.boolQuery()
+					.must(QueryBuilders.matchQuery("interests", "changge"))
+					.mustNot(QueryBuilders.matchQuery("interests", "hejiu"))
+					.should(QueryBuilders.matchQuery("address", "bei jing"))
+					.filter(QueryBuilders.rangeQuery("birthday").gte("1999-01-01").format("yyyy-MM-dd"));
+		SearchResponse sr = client.prepareSearch("blog")
+				.setQuery(qb)
+				.setSize(2)
+				.get();
+		SearchHits hits = sr.getHits();
+		for (SearchHit searchHit : hits) {
+			System.out.println(searchHit.getSourceAsString());
+			Map<String, Object> map = searchHit.getSourceAsMap();
+			for (String key : map.keySet()) {
+				System.out.println(key+"="+map.get(key));
+			}
+		}
+		client.close();
+	}
+	
+	// 分组聚合
+	@Test
+	public void test19() throws Exception {
+		Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.182.101"), 9300));
+		AggregationBuilder agg = AggregationBuilders.terms("terms").field("age");
+		SearchResponse response= client.prepareSearch("index1").addAggregation(agg).execute().actionGet();
+		Terms terms = response.getAggregations().get("terms");
+		for (Terms.Bucket entry : terms.getBuckets()) {
+			System.out.println(entry.getKey()+":"+entry.getDocCount());
+		}
+		client.close();
+	}
+	
+	// 分组过滤
+	@Test
+	public void test20() throws Exception {
+		Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.182.101"), 9300));
+		QueryBuilder qb = QueryBuilders.termQuery("age", 20);
+		AggregationBuilder agg = AggregationBuilders.filter("filter", qb);
+		SearchResponse response= client.prepareSearch("index1").addAggregation(agg).execute().actionGet();
+		Filter filter = response.getAggregations().get("filter");
+		System.out.println(filter.getDocCount());
+		client.close();
+	}
+	@Test
+	public void test21() throws Exception {
+		Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.182.101"), 9300));
+		
+		AggregationBuilder agg = AggregationBuilders.filters("filters", 
+				new FiltersAggregator.KeyedFilter("changge", QueryBuilders.termQuery("interests", "changge")));
+		SearchResponse response= client.prepareSearch("index1").addAggregation(agg).execute().actionGet();
+		Filters filters = response.getAggregations().get("filters");
+		for (Filters.Bucket entry : filters.getBuckets()) {
+			System.out.println(entry.getKey()+":"+entry.getDocCount());
+		}
+		client.close();
+	}
+	//range聚合
+	@Test
+	public void test22() throws Exception {
+		Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.182.101"), 9300));
+		AggregationBuilder agg = AggregationBuilders
+				.range("range")
+				.field("age")
+				.addUnboundedTo(50)//(,to)
+				.addRange(25, 50)//[from,to)
+				.addUnboundedFrom(25);//[from,)
+		SearchResponse response= client.prepareSearch("index1").addAggregation(agg).execute().actionGet();
+		Range range = response.getAggregations().get("range");
+		for (Range.Bucket entry : range.getBuckets()) {
+			System.out.println(entry.getKey()+":"+entry.getDocCount());
+		}
+		client.close();
+	}
+	
+	// missing
+	@Test
+	public void test23() throws Exception {
+		Settings settings = Settings.builder().put("cluster.name", "my-application").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+				.addTransportAddress(new TransportAddress(InetAddress.getByName("192.168.182.101"), 9300));
+		AggregationBuilder agg = AggregationBuilders.missing("missing").field("price");
+		SearchResponse response= client.prepareSearch("index1").addAggregation(agg).execute().actionGet();
+		Aggregation aggregation=response.getAggregations().get("missing");
+		System.out.println(aggregation.toString());
+		client.close();
+	}
 }
